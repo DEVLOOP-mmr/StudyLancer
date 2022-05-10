d9fe307d74c57ba41f6dba9f4ca93b721fd950a4
Dhruv Devasthale <dhruv_devasthale@outlook.com>
2022-05-10 13:42:18 +0530

    add background handler

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:elite_counsel/variables.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,9 +20,23 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import 'widgets/themes.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  NotificationCubit.showLocalNotification(
+    message.notification!.title!,
+    message.notification!.body!,
+  );
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+
+  //FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   if (kDebugMode) {
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
     runApp(DevicePreview(
