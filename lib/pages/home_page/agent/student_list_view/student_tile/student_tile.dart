6f97913f65050b6013a80d097bd6859b3a049599
Dhruv Devasthale <dhruv_devasthale@outlook.com>
2022-05-12 14:54:42 +0530

    add app notif icon+ test for offer progress

--- lib/pages/home_page/agent/student_list_view/student_tile/student_tile.dart
+++ lib/pages/home_page/agent/student_list_view/student_tile/student_tile.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:elite_counsel/bloc/home_bloc/home_bloc.dart';
+import 'package:elite_counsel/pages/home_page/agent/student_list_view/student_tile/application_progress_button.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
@@ -171,64 +172,6 @@ class StudentTile extends StatelessWidget {
               ),
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class ApplicationStatus extends StatefulWidget {
-  const ApplicationStatus({
-    Key? key,
-    required this.application,
-  }) : super(key: key);
-  final Application application;
-
-  @override
-  State<ApplicationStatus> createState() => _ApplicationStatusState();
-}
-
-class _ApplicationStatusState extends State<ApplicationStatus> {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<StudentApplicationCubit, Student>(
-      builder: (context, state) {
-        return Container(
-          key: UniqueKey(),
-          margin: const EdgeInsets.only(right: 5),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white.withOpacity(0.04)),
-          child: DropdownButton(
-              dropdownColor: Variables.backgroundColor,
-              underline: Container(),
-              iconSize: 0,
-              value: state.timeline ?? 0,
-              items: [0, 1, 2, 3, 4, 5]
-                  .map((e) => DropdownMenuItem<int>(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Text(
-                              StudentApplicationCubit
-                                      .parseProgressTitleFromValue(e) ??
-                                  '',
-                              style: const TextStyle(
-                                  color: Variables.accentColor,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold)),
-                        ),
-                        value: e,
-                      ))
-                  .toList(),
-              onChanged: (val) {
-                BlocProvider.of<StudentApplicationCubit>(context)
-                    .changeApplicationProgress(
-                  widget.application.applicationID!,
-                  val as int,
-                );
-                setState(() {});
-                BlocProvider.of<HomeBloc>(context).getHome();
-              }),
         );
       },
     );
